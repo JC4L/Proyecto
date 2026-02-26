a61e3c0609c4713a2665861497e6164ec2b483e9
GNcode <gngalarga027@gmail.com>
2026-02-25 22:43:29 -0500

    refactor: Remove unused repository interfaces DimEnergyTypeRepository and DimEntityRepository

--- backend/api/src/main/java/com/energia/api/repository/DimEnergyTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.energia.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.energia.api.modelo.DimEnergyType;
-
-public interface DimEnergyTypeRepository extends JpaRepository<DimEnergyType, Long> {
-
-}
--- backend/api/src/main/java/com/energia/api/repository/DimEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.energia.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.energia.api.modelo.DimEntity;
-
-public interface DimEntityRepository extends JpaRepository<DimEntity, Long> {
-
-}
